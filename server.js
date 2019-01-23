c8c9801d9e58227185035605c687e1b0e3c67aa5
Razvan Negrea <howl@wolfpilot.co.uk>
2019-01-23 23:54:54 +0100

    Add basic Express server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+// Helpers
+const path = require('path');
+
+// Build
+const express = require('express');
+
+// Server
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './src/index.html'));
+});
+
+app.listen(port);
+
+console.log('Listening on port ' + port);
